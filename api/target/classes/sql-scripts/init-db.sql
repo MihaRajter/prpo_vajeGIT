bce27b4dc5a084aff8c27e94cf0f39d754bd21ea
CapitanPirk <tic.jur@gmail.com>
2019-11-10 21:09:12 +0100

    update4

--- api/target/classes/sql-scripts/init-db.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO uporabnik (ime, priimek) VALUES ('Petra', 'Kos');
-INSERT INTO uporabnik (ime, priimek) VALUES ('1243254', '123');
-INSERT INTO artikel(ime, st_nakupov) VALUES('cokolino',5);
-INSERT INTO artikel(ime, st_nakupov) VALUES('mleko',2);
